4984588f28f675562fdf802ee269802a2891fabf
Adam Quiroz <adam123quiroz@gmail.com>
2020-06-18 18:23:03 -0400

    add functionality

--- src/main/java/ucb/edu/bo/sabicovid19/dao/BiCaseRepository.java
+++ src/main/java/ucb/edu/bo/sabicovid19/dao/BiCaseRepository.java
@@ -1,10 +1,8 @@
 package ucb.edu.bo.sabicovid19.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ucb.edu.bo.sabicovid19.domain.BiCase;
 import ucb.edu.bo.sabicovid19.domain.BiMedicalCondition;
-import ucb.edu.bo.sabicovid19.domain.BiMedicalCondition_;
 
 import java.util.Date;
 import java.util.List;
@@ -19,4 +17,6 @@ public interface BiCaseRepository extends JpaRepository<BiCase, Integer> {
     Integer countAllByUpdateDate(Date date);
 
     Integer countAllByMedCondIdAndUpdateDateAndStatus(BiMedicalCondition medicalCondition, Date dateNow, Integer status);
+
+    Integer countByMunicipallyId_ProvinceId_DepartmentId_DepartmentAndMedCondIdAndStatus(String department, BiMedicalCondition medicalCondition, Integer status);
 }

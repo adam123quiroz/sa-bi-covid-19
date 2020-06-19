6f1ddd8a858b11acf946b6270428c9699795f174
Adam Quiroz <adam123quiroz@gmail.com>
2020-06-19 02:59:56 -0400

    add authentication config telegram bot

--- src/main/java/ucb/edu/bo/sabicovid19/api/BotTelegramController.java
+++ src/main/java/ucb/edu/bo/sabicovid19/api/BotTelegramController.java
@@ -1,9 +1,11 @@
 package ucb.edu.bo.sabicovid19.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ucb.edu.bo.sabicovid19.AuthorizationJwtRest;
 import ucb.edu.bo.sabicovid19.bl.BotTelegramBl;
 import ucb.edu.bo.sabicovid19.model.MessageReportModel;
 import ucb.edu.bo.sabicovid19.model.MessageUser;
@@ -14,19 +16,28 @@ import ucb.edu.bo.sabicovid19.model.MessageUser;
 public class BotTelegramController {
     final BotTelegramBl botTelegramBl;
 
+    @Value("${sa-bi-covid-19.security.secretJwt}")
+    private String secretJwt;
+
     public BotTelegramController(BotTelegramBl botTelegramBl) {
         this.botTelegramBl = botTelegramBl;
     }
 
     @PostMapping
-    public ResponseEntity<MessageReportModel> findAllInformation(@RequestBody MessageUser messageUser) {
-        if (messageUser.getIsDepartment().equalsIgnoreCase("NO")) {
-            return ResponseEntity.ok(this.botTelegramBl.findAllCasesBolivia());
-        }
-        if (messageUser.getIsDepartment().equalsIgnoreCase("SI")){
-            return ResponseEntity.ok(this.botTelegramBl.findAllCasesByDepartment(
-                    messageUser.getPlace()
-            ));
+    public ResponseEntity<MessageReportModel> findAllInformation(@RequestHeader("Authorization") String authorization,
+                                                                 @RequestBody MessageUser messageUser) {
+        AuthorizationJwtRest authorizationJwtRest = new AuthorizationJwtRest(authorization, secretJwt);
+        if (authorizationJwtRest.validateToken()) {
+            if (messageUser.getIsDepartment().equalsIgnoreCase("NO")) {
+                return ResponseEntity.ok(this.botTelegramBl.findAllCasesBolivia());
+            }
+            if (messageUser.getIsDepartment().equalsIgnoreCase("SI")){
+                return ResponseEntity.ok(this.botTelegramBl.findAllCasesByDepartment(
+                        messageUser.getPlace()
+                ));
+            }
+        } else {
+            return new ResponseEntity<>(new MessageReportModel(), HttpStatus.CONFLICT);
         }
         return new ResponseEntity<>(new MessageReportModel(), HttpStatus.BAD_REQUEST);
     }

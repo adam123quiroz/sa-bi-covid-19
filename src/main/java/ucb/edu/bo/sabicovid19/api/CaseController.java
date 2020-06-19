6725d16c615185993f5d125927b00432ed733d00
Rodolfo Catunta <rodolfo.catunta.uturunco@gmail.com>
2020-06-18 22:02:34 -0400

    Support for CORS in CaseController

--- src/main/java/ucb/edu/bo/sabicovid19/api/CaseController.java
+++ src/main/java/ucb/edu/bo/sabicovid19/api/CaseController.java
@@ -1,7 +1,9 @@
 package ucb.edu.bo.sabicovid19.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +13,11 @@ import ucb.edu.bo.sabicovid19.dto.CaseDto;
 import ucb.edu.bo.sabicovid19.model.CaseModel;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/register-case")
 public class CaseController {
     final
@@ -40,11 +44,16 @@ public class CaseController {
     @PostMapping
     public ResponseEntity<CaseModel> createCustomer(@RequestBody CaseModel caseDto,
                                                     @RequestHeader("Authorization") String authorization) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Credentials", "true");
+        responseHeaders.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
         AuthorizationJwtRest authorizationJwtRest = new AuthorizationJwtRest(authorization, secretJwt);
         if (authorizationJwtRest.validateToken()) {
-            return new ResponseEntity<>(this.caseBl.createCase(caseDto), HttpStatus.CREATED);
+            return ResponseEntity.ok().headers(responseHeaders).body(this.caseBl.createCase((caseDto)));
+            //return new ResponseEntity<>(this.caseBl.createCase(caseDto), HttpStatus.CREATED);
         }
-        return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        return ResponseEntity.status(403).headers(responseHeaders).body(null);
+        //return new ResponseEntity<>(null, HttpStatus.CONFLICT);
     }
 
 }
